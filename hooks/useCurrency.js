1f5a4c0a24161aaca81765be3edff949b47d9ef2
Filip Stojanovic <philsty92@gmail.com>
2023-08-16 04:34:50 -0400

    Updated useCurrency with temp values

--- hooks/useCurrency.js
+++ hooks/useCurrency.js
@@ -5,14 +5,18 @@ const BASE_URL =
 	'http://api.exchangeratesapi.io/v1/latest?access_key=8aaddd8db0610550895e7f8551c574eb&symbols=USD,INR,GBP'
 
 export const useCurrency = () => {
-	const [currencies, setCurrencies] = useState([])
+	const [currencies, setCurrencies] = useState(['EUR', 'USD', 'INR', 'GBP'])
 	const [data, setData] = useState()
+
+	const tempData = { USD: 1.092598, INR: 90.841878, GBP: 0.857285, EUR: 1 }
 
 	const convert = (fromValue, toValue, amount) => {
 		const from =
-			data && Object.entries(data).filter(([key, value]) => key == fromValue)
+			tempData &&
+			Object.entries(tempData).filter(([key, value]) => key == fromValue)
 		const to =
-			data && Object.entries(data).filter(([key, value]) => key == toValue)
+			tempData &&
+			Object.entries(tempData).filter(([key, value]) => key == toValue)
 
 		return (
 			from &&
@@ -26,14 +30,14 @@ export const useCurrency = () => {
 		)
 	}
 
-	useEffect(() => {
-		fetch(BASE_URL)
-			.then(res => res.json())
-			.then(data => {
-				setCurrencies([data.base, ...Object.keys(data.rates)]),
-					setData({ ...data.rates, EUR: 1 })
-			})
-	}, [])
+	// useEffect(() => {
+	// 	fetch(BASE_URL)
+	// 		.then(res => res.json())
+	// 		.then(data => {
+	// 			setCurrencies([data.base, ...Object.keys(data.rates)]),
+	// 				setData({ ...data.rates, EUR: 1 })
+	// 		})
+	// }, [])
 
 	return { currencies, convert }
 }
