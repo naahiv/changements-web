ac708d110c24bc786f62ee4ba0b3a71468515232
Filip Stojanovic <philsty92@gmail.com>
2023-08-16 04:06:47 -0400

    Updated url in currency hook.

--- hooks/useCurrency.js
+++ hooks/useCurrency.js
@@ -2,7 +2,7 @@
 import { useState, useEffect } from 'react'
 
 const BASE_URL =
-	'http://api.exchangeratesapi.io/v1/latest?access_key=8aaddd8db0610550895e7f8551c574eb&symbols=USD,INR,GBP'
+	'https://api.exchangeratesapi.io/v1/latest?access_key=8aaddd8db0610550895e7f8551c574eb&symbols=USD,INR,GBP'
 
 export const useCurrency = () => {
 	const [currencies, setCurrencies] = useState([])
